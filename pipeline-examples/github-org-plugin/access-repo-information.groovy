204bd15eab704c715d0be4d9bf0fb514b0e0d232
davidwalter0 <walter.david@gmail.com>
2016-06-07 16:25:52 -0400

    branches with slash separators
    slashes can be used in git branch names. 
    collect all of the tokens after 1

--- pipeline-examples/github-org-plugin/access-repo-information.groovy
+++ pipeline-examples/github-org-plugin/access-repo-information.groovy
@@ -5,4 +5,6 @@
     org = tokens[0]
     repo = tokens[1]
     branch = tokens[2]
-
+    // maybe we have a branch with slash separators?
+    println(tokens[2..-1].join('/'))
+    println("o: ${org}\nr: ${repo}\nb: ${branch}\ntokens: ${tokens[2..-1].join('/')}");    
